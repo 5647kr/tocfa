2a2cfc1440f354a439ccf281330130fe00ec6770
K's_FE <5647kr@gmail.com>
2025-01-19 16:13:41 +0900

    [🔨 Refactor] Finance 상태값 로컬스토리지 저장 기능

--- finance/finance.js
+++ finance/finance.js
@@ -4,9 +4,9 @@ class Finance {
     this.totalFinance = finance.querySelector(".totalFinance h2");
     this.financeForm = finance.querySelector(".financeInput form");
     this.financeType = this.financeForm.querySelectorAll(".financeType input[type=radio]");
-    this.financeName = this.financeForm.querySelector(".financeInputWrap .financeName")
+    this.financeTitle = this.financeForm.querySelector(".financeInputWrap .financeTitle")
     this.financePrice = this.financeForm.querySelector(".financeInputWrap .financePrice")
-    this.regFinance = this.financeForm.querySelector(".regBtn");
+    this.regBtn = this.financeForm.querySelector(".regBtn");
 
     this.earnFinance = finance.querySelector(".earnFinance strong");
     this.paidFinance = finance.querySelector(".paidFinance strong");
@@ -16,23 +16,24 @@ class Finance {
     this.financeArr = JSON.parse(localStorage.getItem("financeArr")) || [];
     this.financeId = JSON.parse(localStorage.getItem("financeId")) || 0;
     this.editFinanceId = null;
+
+    this.currentDate = null;
+
+    this.financeState = JSON.parse(localStorage.getItem("financeState")) || []
   }
 
   financeEvent() {
-    this.displayFinanceList();
     this.validateInput();
 
-    this.regFinance.addEventListener("click", (e) => {
-      e.preventDefault();
-      
-      this.regFinance.value === "register" ? this.regFinanceEvent() : this.updateFinanceEvent(this.editFinanceId);
-    })
+    this.manageFinanceData();
+    this.displayFinanceList();
+    this.displayFinanceState();
   }
 
   validateInput() {
-    this.financeName.addEventListener("input", () => {
-      if(this.financeName.value.length > 10) {
-        this.financeName.value = this.financeName.value.slice(0, 10)
+    this.financeTitle.addEventListener("input", () => {
+      if(this.financeTitle.value.length > 10) {
+        this.financeTitle.value = this.financeTitle.value.slice(0, 10)
       }
     })
 
@@ -44,64 +45,100 @@ class Finance {
       }
     })
   }
-  
-  regFinanceEvent() {
-    const date = new Date();
-    const month = date.getMonth() + 1;
-    const day = date.getDate();
-    let type;
-    const name = this.financeName.value.trim();
-    const price = this.financePrice.value.trim();
 
+  manageDate() {
+    const newDate = new Date();
+    const month = newDate.getMonth();
+    const date = newDate.getDate();
 
+    this.currentDate = `${month + 1}/${date}`;
+  }
 
+  manageFinanceData() {
+    this.manageDate();
+
+    this.regBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      
+      const id = this.financeId;
+      const date = this.currentDate;
+      let type = null;
+      const title = this.financeTitle.value.trim();
+      const price = parseInt(this.financePrice.value.trim());
+
+      const selectedType = Array.from(this.financeType).find((item) => item.checked);
+      type = selectedType ? selectedType.value : null;
+
+      const props = { id, date, type, title, price }
+
+      this.financeState.length === 0 ? this.saveFinanceState() : this.updateFinanceState();
+
+      if(this.editFinanceId === null) {
+        this.regFinance(props);
+        this.updateFinanceState();
+      } else {
+        this.updateFinance(this.editFinanceId);
+      }
+    })
+
+    if(this.financeArr !== 0 && this.currentDate.split("/")[1] === "19") {
+      const newMonthFinanceArr = this.financeArr.some((item) => item.date.split("/")[0] !== this.currentDate.split("/")[0]);
+
+      if(newMonthFinanceArr) {
+        this.manageFinanceList();
+        this.updateFinanceState();
+      }
+    } else {
+      return;
+    }
+  }
+
+  regFinance(props) {
     // validation: financeName이 비어있거나 10글자 이상일 경우
-    if (name === "" || name.length > 10) {
+    if (props.title === "" || props.title.length > 10) {
       alert("이름은 1자 이상 10자 이하로 입력해주세요.");
       return; // 입력값이 유효하지 않으면 함수 종료
     }
 
     // validation: financePrice가 비어있거나 숫자가 아닐 경우
-    if (price === "" || isNaN(price)) {
+    if (props.price === "" || isNaN(props.price)) {
       alert("가격은 숫자로 입력해주세요.");
       return; // 입력값이 유효하지 않으면 함수 종료
     }
 
     // validation: 가격은 음수나 0을 허용하지 않도록
-    if (parseFloat(price) <= 0) {
+    if (parseInt(props.price) <= 0) {
       alert("가격은 0보다 큰 숫자여야 합니다.");
       return;
     }
 
-    const selectedType = Array.from(this.financeType).find((item) => item.checked);
-    type = selectedType ? selectedType.value : null;
-
-    const financeItem = {
-      id: this.financeId,
-      date: `${month}/${day}`,
-      type: type,
-      name: name,
-      price: price
+    const financeItemData = {
+      id: props.id,
+      date: props.date,
+      type: props.type,
+      title: props.title,
+      price: props.price
     }
 
-    this.financeArr.push(financeItem);
+    this.financeArr.push(financeItemData);
 
     localStorage.setItem("financeArr", JSON.stringify(this.financeArr))
     localStorage.setItem("financeId", JSON.stringify(++this.financeId))
 
-    this.displayFinanceList();
-    this.resetInputForm();
+    this.resetInput();
+
+    this.createFinanceItem(financeItemData);
   }
 
-  createListItem(item) {
+  createFinanceItem(financeItemData) {
     const financeItem = document.createElement("li");
     const financeItemContent = `
-      <time class="date">${item.date}</time>
-      <p class="name">${item.name}</p>
+      <time class="date">${financeItemData.date}</time>
+      <p class="name">${financeItemData.title}</p>
       <p class="price">
-        ${item.type === "earn" ? `+${parseInt(item.price).toLocaleString()}` : `-${parseInt(item.price).toLocaleString()}`}
+        ${financeItemData.type === "earn" ? `+${parseInt(financeItemData.price).toLocaleString()}` : `-${parseInt(financeItemData.price).toLocaleString()}`}
       </p>
-      <div>
+      <div class="btnWrap">
         <button class="editBtn">
           <i class="fa-solid fa-pencil"></i>
         </button>
@@ -109,45 +146,68 @@ class Finance {
           <i class="fa-solid fa-trash"></i>
         </button>
       </div>
-    `
+    `;
 
-    financeItem.id = item.id;
+    financeItem.id = financeItemData.id;
     financeItem.innerHTML = financeItemContent;
-    this.financeList.append(financeItem);
 
+    this.financeList.appendChild(financeItem);
     this.manageFinanceItem(financeItem);
   }
 
   displayFinanceList() {
-    this.financeList.innerHTML = "";  // 기존 리스트 초기화
-    const today = new Date();
-    const newMonth = today.getMonth() + 1;
-  
-    // 이번 달의 financeItem들만 필터링
-    const thisMonthFinance = this.financeArr.filter((financeItem) => {
-      const monthFinance = parseInt(financeItem.date.split("/")[0]);
-      return monthFinance === newMonth;
-    });
-  
-    // 필터링된 항목들을 리스트에 추가
-    thisMonthFinance.forEach((financeItem) => {
-      this.createListItem(financeItem);
-    });
-  
-    // 이번 달 항목들만을 전달하여 재정 상태 계산
-    this.calculateFinance(thisMonthFinance);
+    this.financeList.innerHTML = "";
+
+    this.financeArr.forEach((item) => {
+      this.createFinanceItem(item);
+    })
   }
 
   manageFinanceItem(financeItem) {
-    const editBtn = financeItem.querySelector(".editBtn");
-    const deleteBtn = financeItem.querySelector(".deleteBtn");
+    const editBtn = financeItem.querySelector(".btnWrap .editBtn");
+    const delBtn = financeItem.querySelector(".btnWrap .deleteBtn");
 
-    editBtn.addEventListener("click", () => this.editFinanceItem(financeItem.id))
-    deleteBtn.addEventListener("click", () => this.deleteFinanceItem(financeItem.id))
+    editBtn.addEventListener("click", () => this.editFinance(financeItem.id))
+    delBtn.addEventListener("click", () => this.deleteFinance(financeItem.id))
   }
 
-  // financeItem 수정 준비 함수
-  deleteFinanceItem(id) {
+  editFinance(id) {
+    const editItem = this.financeArr.find(item => item.id === parseInt(id));
+    
+    if(editItem) {
+      this.financeType.value = editItem.type;
+      this.financeTitle.value = editItem.title;
+      this.financePrice.value = editItem.price;
+      
+      this.editFinanceId = editItem.id
+    }
+  }
+
+  updateFinance(id) {
+    const updateItem = this.financeArr.find(item => item.id === parseInt(id));
+
+    if(updateItem) {
+      let type = null;
+      const selectedType = Array.from(this.financeType).find((item) => item.checked);
+      type = selectedType ? selectedType.value : null;
+
+      updateItem.type = type;
+      updateItem.title = this.financeTitle.value;
+      updateItem.price = parseInt(this.financePrice.value);
+
+      localStorage.setItem("financeArr", JSON.stringify(this.financeArr));
+
+      this.displayFinanceList();
+
+      this.editFinanceId = null
+
+      this.updateFinanceState();
+
+      this.resetInput();
+    }
+  }
+
+  deleteFinance(id) {
     this.financeArr = this.financeArr.filter(item => item.id !== parseInt(id))
 
     localStorage.setItem("financeArr", JSON.stringify(this.financeArr));
@@ -158,78 +218,98 @@ class Finance {
     }
 
     this.displayFinanceList();
+
+    this.updateFinanceState();
+  }
+  
+  calculateFinance() {
+    const earnPrice = [];
+    const paidPrice = [];
+
+    this.financeArr.forEach((item) => {
+      if(item.type === "earn") {
+        earnPrice.push(item.price)
+      } else {
+        paidPrice.push(item.price)
+      }
+    })
+
+    const totalEarn = earnPrice.reduce((acc, price) => acc +  price, 0);
+    const totalPaid = paidPrice.reduce((acc, price) => acc +  price, 0);
+    const totalAmount = totalEarn - totalPaid;
+
+    return { totalEarn, totalPaid, totalAmount }
   }
 
-  // financeItem 수정 준비 함수
-  editFinanceItem(id) {
-    const editItem = this.financeArr.find(item => item.id === parseInt(id));
+  saveFinanceState() {
+    const financeStateData = {
+      "totalEarn": 0,
+      "totalPaid": 0,
+      "totalAmount": 0
+    };
 
-    if(editItem) {
-      this.financeType.value = editItem.type;
-      this.financeName.value = editItem.name;
-      this.financePrice.value = editItem.price;
-
-      this.regFinance.value = "edit"
-
-      this.editFinanceId = editItem.id
+    for(let i = 0; i <= 11; i++) {
+      this.financeState.push({ ... financeStateData });
     }
+
+    localStorage.setItem("financeState", JSON.stringify(this.financeState));
   }
 
-  // 수정한 finance 로컬 스토리지에 저장하여 반영
-  updateFinanceEvent(id) {
-    const updateItem = this.financeArr.find(item => item.id === parseInt(id));
+  updateFinanceState() {
+    const newDate = new Date();
+    const stateMonth = newDate.getMonth();
+    let updateState = this.financeState[stateMonth];
 
-    if(updateItem) {
-      updateItem.type = this.financeType.value;
-      updateItem.name = this.financeName.value;
-      updateItem.price = this.financePrice.value;
+    if(updateState) {
+      updateState = {
+        "totalEarn": 0,
+        "totalPaid": 0,
+        "totalAmount": 0
+      };
 
-      localStorage.setItem("financeArr", JSON.stringify(this.financeArr));
+      // `calculateFinance` 실행하여 계산된 값 가져오기
+      const { totalEarn, totalPaid, totalAmount } = this.calculateFinance();
 
-      this.displayFinanceList();
+      // 월별 재정 상태 업데이트
+      this.financeState[stateMonth] = {
+        totalEarn,
+        totalPaid,
+        totalAmount
+      };
 
-      this.regFinance.value = "register";
-
-      this.editFinanceId = null
-
-      this.resetInputForm();
+      // 로컬스토리지에 업데이트된 상태 저장
+      localStorage.setItem("financeState", JSON.stringify(this.financeState));
     }
+
+    this.displayFinanceState();
+  }
+
+  displayFinanceState() {
+    const newDate = new Date();
+    const stateMonth = newDate.getMonth();
+    let financeState = this.financeState[stateMonth];
+
+    this.totalFinance.textContent = `${(financeState.totalAmount).toLocaleString()}`;
+    this.earnFinance.textContent = `${(financeState.totalEarn).toLocaleString()}`;
+    this.paidFinance.textContent = `${(financeState.totalPaid).toLocaleString()}`;
+
+    parseInt(this.totalFinance.textContent) < 0 ? this.totalFinance.classList.add("minus") : this.totalFinance.classList.remove("minus")
+  }
+
+  manageFinanceList() {
+    const newMonth = new Date().getMonth() + 1;
+    this.financeArr = this.financeArr.filter(item => parseInt(item.date.split("/")[0] !== newMonth))
+
+    localStorage.setItem("financeArr", JSON.stringify(this.financeArr));
   }
 
   // 인풋 초기화 함수
-  resetInputForm() {
-    this.financeName.value = "";
+  resetInput() {
+    this.financeTitle.value = "";
     this.financePrice.value = "";
   }
 
-  // financeItem 계산하여 총값 표시하는 함수
-  calculateFinance(filteredItems) {
-    const earnPrices = [];
-    const paidPrices = [];
-  
-    // 필터링된 항목들을 순회하며 수입과 지출 가격을 구분하여 배열에 담음
-    filteredItems.forEach(item => {
-      if (item.type === "earn") {
-        earnPrices.push(parseFloat(item.price));  // 수입은 양수로
-      } else if (item.type === "paid") {
-        paidPrices.push(parseFloat(item.price));  // 지출은 음수로
-      }
-    });
-  
-    // 수입과 지출 각각의 합 계산
-    const totalEarn = earnPrices.reduce((acc, price) => acc + price, 0);
-    const totalPaid = paidPrices.reduce((acc, price) => acc + price, 0);
-  
-    // 총 재정 상태 계산
-    const totalFinance = totalEarn - totalPaid;
 
-    totalFinance >= 0 ? this.totalFinance.style.color = "var(--sub-color)" : this.totalFinance.style.color = "var(--error-color)"
-  
-    // 화면에 출력
-    this.earnFinance.textContent = `+${parseInt(totalEarn).toLocaleString()}`;
-    this.paidFinance.textContent = `-${parseInt(totalPaid).toLocaleString()}`;
-    this.totalFinance.textContent = `${parseInt(totalFinance).toLocaleString()}`;
-  }
 }
 
 const finance = new Finance();
