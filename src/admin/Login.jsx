c025bb382af25040b1ee27d791fa69615f08978b
K's_FE <5647kr@gmail.com>
2025-07-30 16:48:49 +0900

    [🔨 Refactor] admin_login 컴포넌트화 리팩토링

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -3,15 +3,17 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import LoginApi from "../api/LoginApi";
 import MainWrap from "../components/Main";
+import { TextInput } from "../components/Input";
+
 
 export default function Login() {
-  const [errorMsg, setErrorMsg] = useState("");
+  const [isError, setIsError] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setErrorMsg("");
-
+    setIsError("");
+    
     const formData = new FormData(e.currentTarget);
     const loginData = Object.fromEntries(formData.entries());
 
@@ -19,30 +21,24 @@ export default function Login() {
       await LoginApi(loginData);
       navigate("/admin");
     } catch (error) {
-      setErrorMsg("이메일 또는 비밀번호가 잘못되었습니다.");
+      setIsError("이메일 또는 비밀번호가 잘못되었습니다.");
     }
   };
 
   return (
     <MainWrap>
       <LoginWrap>
-        <LoginForm>
-          <h1>
-            <img src="assets/img/logo.png" alt="tocfa logo" />
-          </h1>
-          <form onSubmit={handleSubmit}>
-            <InputWrap>
-              <label htmlFor="email">이메일</label>
-              <input type="email" name="email" id="email" required />
-            </InputWrap>
-            <InputWrap>
-              <label htmlFor="password">비밀번호</label>
-              <input type="password" name="password" id="password" required />
-            </InputWrap>
+        <div>
+          <h2>
+            <img src="/assets/img/logo.png" alt="tocfa logo" />
+          </h2>
+          <LoginForm onSubmit={handleSubmit}>
+            <TextInput id="email" type="email" name="email" placeholder="이메일을 입력하세요" >이메일</TextInput>
+            <TextInput id="password" type="password" name="password" placeholder="비밀번호를 입력하세요" >비밀번호</TextInput>
             <button type="submit">로그인</button>
-            {errorMsg ? <strong>{errorMsg}</strong> : null}
-          </form>
-        </LoginForm>
+          </LoginForm>
+          {isError? <strong>{isError}</strong> : null}
+        </div>
       </LoginWrap>
     </MainWrap>
   );
@@ -53,42 +49,32 @@ const LoginWrap = styled.section`
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
-`;
-
-const LoginForm = styled.div`
-  border: 1px solid var(--shadow-color);
-  padding: 20px 40px 40px;
-  border-radius: 1rem;
-  & img {
-    display: block;
-    margin: 0 auto 4rem;
+  & > div {
+    border: 1px solid var(--shadow-color);
+    border-radius: 1rem;
+    padding: 2rem 4rem 4rem;
+  }
+  & h2 {
+    text-align: center;
+    margin-bottom: 4rem;
+  }
+  & h2 img {
     width: 3rem;
     aspect-ratio: 1 / 1;
+    vertical-align: top;
   }
-  & form {
-    display: flex;
-    flex-direction: column;
-    gap: 2rem;
-  }
-
-  & form button {
-    padding: 1rem 0;
+  & strong {
+    display: block;
     margin-top: 2rem;
-  }
-
-  & form button:active {
-    box-shadow: inset 0 2px 4px var(--shadow-color);
-  }
-
-  & form strong {
-    color: red;
+    color: var(--error-color);
   }
 `;
 
-const InputWrap = styled.div`
+const LoginForm = styled.form`
   display: flex;
   flex-direction: column;
-  & > input {
-    padding: 10px;
+  gap: 20px;
+  & button {
+    margin-top: 20px;
   }
-`;
+`
