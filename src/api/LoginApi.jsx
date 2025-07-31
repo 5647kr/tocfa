76d670be18f2cb709e45631a94838b66ba9e8712
K's_FE <5647kr@gmail.com>
2025-07-31 15:51:31 +0900

    [🔨 Refactor] admin_Login 로그인해도 이동하지 않는 문제 발견하여 코드 수정

--- src/api/LoginApi.jsx
+++ src/api/LoginApi.jsx
@@ -2,11 +2,12 @@ import supabase from "../supabaseClient";
 
 export default async function LoginApi(loginData) {
   try {
-    const { error } = await supabase.auth.signInWithPassword(loginData);
+    const { data, error } = await supabase.auth.signInWithPassword(loginData);
 
     if (error) {
       throw new Error("로그인 오류 발생");
     }
+    return data;
   } catch (error) {
     throw error;
   }
