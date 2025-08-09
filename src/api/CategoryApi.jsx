be2a0ab1a8e7b5474efef6b7eca26f6a7b3c2d38
K's_FE <5647kr@gmail.com>
2025-08-09 16:29:40 +0900

    Refactor: compnents_categoryApi  기능구현

--- src/api/CategoryApi.jsx
+++ src/api/CategoryApi.jsx
@@ -1,12 +1,14 @@
-import supabase from '../supabaseClient'
+import supabase from "../supabaseClient";
 
-
-export default async function CategoryApi({userType}) {
+export default async function CategoryApi(userType) {
   try {
-    const {data, error} = await supabase.from("category").select("*").eq("userType", userType)
+    const { data, error } = await supabase
+      .from("category")
+      .select("*")
+      .eq("userType", userType);
 
-    if(error) {
-      throw new Error("네트워크에 오류가 있음")
+    if (error) {
+      throw new Error("네트워크에 오류가 있음");
     }
     return data;
   } catch (error) {
