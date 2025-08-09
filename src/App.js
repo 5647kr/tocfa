f03dae9ae9a88580fa5ada51d2ebc54903059433
K's_FE <5647kr@gmail.com>
2025-08-09 16:30:23 +0900

    Feat: components_Router 설정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import GlobalStyle from "./styles/GlobalStyle";
+import AdminHome from "./admin/AdminHome";
+import UserHome from "./user/UserHome";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        {/* <Route path="/admin" element={<AdminHome />} />
+        <Route path="/" element={<UserHome />} /> */}
+      </Routes>
+    </>
   );
 }
 export default App;
