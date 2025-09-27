12fa58f935132ff8bd54df41bfd6ffca7fa96915
5647kr <5647kr@gmail.com>
2025-09-27 17:05:49 +0900

    Del: 불필요 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./style/GlobalStyle";
-import LoginPage from "./admin/LoginPage";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
       <GlobalStyle />
-
-      <Routes>
-        <Route path="/admin/login" element={<LoginPage />} />
-      </Routes>
-    </BrowserRouter>
+    </>
   );
 }
--- src/style/GlobalStyle.jsx
+++ src/style/GlobalStyle.jsx
@@ -4,13 +4,12 @@ const GlobalStyle = createGlobalStyle`
   :root {
     --bg-color: #D1D5DC;
     --text-color: #0F172B;
-    --error-color: #FB2C36;
     --boxBg-color: #45556C;
     --stroke-color: #99A1AF;
+    --error-color: #FB2C36;
     --errorBg-color: #FFE2E2;
     --black-color: #000000;
     --white-color: #ffffff;
-    --font-ssz: clamp(1.2rem, 1.3vw, 2rem);
     --font-rw: 400;
     --font-mw: 500;
     --font-bw: 700;
@@ -53,58 +52,9 @@ const GlobalStyle = createGlobalStyle`
     text-decoration: none;
     cursor: pointer;
   }
-  
-
-  h1 {
-    font-size: clamp(2.6rem, 3vw, 3.4rem);
-  }
-  h2 {
-    font-size: clamp(2.2rem, 2.5vw, 3rem);
-  }
-  h3, h4 {
-    font-size: clamp(1.8rem, 2vw, 2.6rem);
-  }
-  p, button, a, input, textarea, label, strong, span, th, pre {
-    font-size: clamp(1.4rem, 1.6vw, 2.2rem);
-  }
-
-  svg, img {
-    width: clamp(1.4rem, 1.6vw, 2.2rem);
-  }
 
   &:where(input, textarea, a, button):focus {
     outline: none;
-  }
-  
-  main {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 1.6rem;
-    padding-inline: 1.6rem;
-  }
-
-  @media screen and (max-width: 480px) {
-    main {
-      grid-template-columns: repeat(4, 1fr);
-      gap: 1.6rem;
-      padding-inline: 1.6rem;
-    }
-  }
-
-  @media screen and (min-width: 481px) and (max-width: 768px) {
-    main {
-      grid-template-columns: repeat(8, 1fr);
-      gap: 2rem;
-      padding-inline: 2rem;
-    }
-  }
-
-  @media screen and (min-width: 769px) {
-    main {
-      grid-template-columns: repeat(12, 1fr);
-      gap: 2.4rem;
-      padding-inline: 2.4rem;
-    }
   }
 `;
 
